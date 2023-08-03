2101274492738cfdeab29ee69db10f8a30ae499d
Peter Njuguna <Ptahbn@gmail.com>
2023-08-03 21:06:35 +0300

    Modify code to be responsive.

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,7 +3,7 @@ import styles from "./page.module.css";
 import Hero from "public/hero.png";
 import Button from "@/components/Button/Button";
 
-export default function Home() {
+const Home = () => {
   return (
     <div className={styles.container}>
       <div className={styles.item}>
@@ -14,11 +14,13 @@ export default function Home() {
           Turning your Idea into Reality. We bring together the teams from the
           global tech industry.
         </p>
-        <Button url="/portfolio" text="See Our Works"/>
+        <Button url="/portfolio" text="See Our Works" />
       </div>
       <div className={styles.item}>
         <Image src={Hero} alt="" className={styles.img} />
       </div>
     </div>
   );
-}
+};
+
+export default Home;
